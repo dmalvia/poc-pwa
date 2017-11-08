9dbd4df76c96be64767802f6e3f59e1c0944a695
tsgodara <tsgodara@gmail.com>
2017-11-08 10:21:18 +0530

    app file changes

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-var express = require('express');
+var express = require("express");
 var app = express();
-app.use(express.static(__dirname + '/app'));
+app.use(express.static(__dirname));
+// console.log('test2'+test)
+// console.log('test'+ express.static(__dirname + '/app'));
 var port = process.env.PORT || 3000;
 // app.listen(port);
 app.listen(port, function() {
-    console.log("Server is running at port " + port);
+  console.log("Server is running at port " + port);
 });
